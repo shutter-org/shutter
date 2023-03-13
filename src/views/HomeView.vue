86dbfb7234e64d2ca3cab03cc8874d8790432637
Alex Prudhomme <prudhomme.alex42@gmail.com>
2023-03-13 00:28:21 -0400

    add search modal ui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,7 @@
   <div class="home">
     <h1>This is an home page</h1>
     <img class="w-10 h-10 rounded"
-      src="https://cdn.discordapp.com/attachments/1069318680736964628/1072638173034852463/images.png"
-      alt="Default avatar" />
-    <LogoAnimation> </LogoAnimation>
+      src="https://cdn.discordapp.com/attachments/1069318680736964628/1072638173034852463/images.png" />
   </div>
 </template>
 
