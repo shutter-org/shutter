c18e56ef59262819f660f97275e9a5f33b992d32
Jérôme <jerome.levesque.14@ulaval.ca>
2023-03-15 16:23:00 -0400

    refactor : RatingInterface.vue from option api to composition

--- src/components/RatingInterface.vue
+++ src/components/RatingInterface.vue
@@ -1,36 +1,29 @@
 <template>
   <div class="flex flex-row gap-2">
     <button>
-      <UpvoteIcon v-if="user_rate !== 1" @click="this.$emit('voteUp')" />
-      <FilledUpvoteIcon v-else @click="this.$emit('voteUp')" />
+      <UpvoteIcon v-if="props.user_rate !== 1" @click="emit('voteUp')" />
+      <FilledUpvoteIcon v-else @click="emit('voteUp')" />
     </button>
-    <p class="font-bold text-xl">{{ total_rate }}</p>
+    <p class="font-bold text-xl">{{ props.total_rate }}</p>
     <button>
-      <DownvoteIcon v-if="user_rate !== -1" @click="this.$emit('voteDown')" />
-      <FilledDownvoteIcon v-else @click="this.$emit('voteDown')" />
+      <DownvoteIcon v-if="props.user_rate !== -1" @click="emit('voteDown')" />
+      <FilledDownvoteIcon v-else @click="emit('voteDown')" />
     </button>
   </div>
 </template>
 
-<script>
+<script setup>
 import UpvoteIcon from "@/components/icons/UpvoteIcon.vue";
 import DownvoteIcon from "@/components/icons/DownvoteIcon.vue";
 import FilledUpvoteIcon from "@/components/icons/filledUpvoteIcon.vue";
 import FilledDownvoteIcon from "@/components/icons/filledDownvoteIcon.vue";
 
-export default {
-  name: "RatingInterface",
-  components: {
-    FilledDownvoteIcon,
-    FilledUpvoteIcon,
-    DownvoteIcon,
-    UpvoteIcon,
-  },
-  props: {
-    total_rate: Number,
-    user_rate: Number,
-  },
-};
+const props = defineProps({
+  total_rate: Number,
+  user_rate: Number,
+});
+
+const emit = defineEmits(["voteUp", "voteDown"]);
 </script>
 
 <style scoped></style>
