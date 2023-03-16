695bb081ddc5fb78c859561aa8caf6161882bcba
Alex Prudhomme <prudhomme.alex42@gmail.com>
2023-03-16 13:27:46 -0400

    Revert "style: publication modal format"
    This reverts commit b26e7d2f380b4b07c8d437c53dd789b69cc31372.

--- src/components/modals/PublicationModal.vue
+++ src/components/modals/PublicationModal.vue
@@ -1,23 +1,12 @@
 <template>
   <div class="fixed inset-0 z-50 flex justify-center overflow-y-scroll">
-    <div
-      class="fixed inset-0 bg-gray-900 bg-opacity-50 overflow-y-auto h-full w-full py-10"
-      @click="emit('close')"
-    ></div>
-    <div
-      class="relative shadow-lg w-full mx-auto max-w-2xl translate-x-[126px] PRO:translate-x-0 PRO:mx-4"
-    >
-      <publication-component
-        class=""
-        @vote-up-pub="emit('voteUpPub')"
-        @vote-down-pub="emit('voteDownPub')"
-        @vote-up-comment="emit('voteUpComment', $event)"
-        @vote-down-comment="emit('voteDownComment', $event)"
-        @search-tag="emit('searchTag', $event)"
-        @add-to-gallery="emit('addToGallery')"
+    <div class="fixed inset-0 bg-gray-900 bg-opacity-50 overflow-y-auto h-full w-full py-10" @click="emit('close')"></div>
+    <div class="relative shadow-lg w-full mx-auto max-w-2xl translate-x-[126px] PRO:translate-x-0 PRO:mx-4">
+      <publication-component class="" @vote-up-pub="emit('voteUpPub')" @vote-down-pub="emit('voteDownPub')"
+        @vote-up-comment="emit('voteUpComment', $event)" @vote-down-comment="emit('voteDownComment', $event)"
+        @search-tag="emit('searchTag', $event)" @add-to-gallery="emit('addToGallery')"
         @add-comment="(publicationId: string, message: string) => emit('addComment', message)"
-        :publication="props.publication"
-      ></publication-component>
+        :publication="props.publication"></publication-component>
       <div class="h-10"></div>
     </div>
   </div>
