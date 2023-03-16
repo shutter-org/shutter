8d5b66c7cf1692c1e5833935bcfd69714eeda222
Alex Prudhomme <prudhomme.alex42@gmail.com>
2023-03-16 16:08:05 -0400

    feat: explore view with publication model

--- src/views/ExploreView.vue
+++ src/views/ExploreView.vue
@@ -1,31 +1,38 @@
 <template>
-  <div class="p-10 max-w-5xl ml-auto mr-auto flex flex-col min-h-screen PRO:min-h-[calc(100vh-160px)]">
-    <div class="mb-10 w-64 rounded-md h-12 border-2 input-color pl-4 self-center flex-row flex items-center border-color">
-      <input class="z-2 outline-none input-color box-border w-full h-11 rounded-md" v-focus placeholder="Search..." />
-      <CrossIcon class="h-6 pr-2" />
-    </div>
-    <div class="flex flex-wrap justify-center gap-4">
-      <button class="w-52 h-52 rounded-lg" v-for="(post, index) in posts" :key="index">
-        <img @click="openPublicationModal(index)" class="w-full object-cover aspect-square rounded-lg"
-          :src="post.picture" />
-        <PublicationModal v-if="showVisitModals[index]" @close="closePublicationModal(index)" />
-      </button>
+  <div id="explore"
+    class=" min-h-screen PRO:min-h-[calc(100vh-160px)] w-full p-10 max-w-7xl ml-auto mr-auto flex flex-col gap-8">
+    <div class="profile w-full flex flex-col gap-2 items-center rounded-lg p-4 border-2 border-color h-full">
+      <div
+        class="my-10 w-64 rounded-md h-12 border-2 input-color pl-4 self-center flex-row flex items-center border-color">
+        <input class="z-2 outline-none input-color box-border w-full h-11 rounded-md" v-focus placeholder="Search..." />
+        <CrossIcon class="h-6 pr-2" />
+      </div>
+      <div class="flex flex-wrap justify-center gap-4">
+        <button class="w-52 h-52 rounded-lg" v-for="post in posts" @click="openPublicationModal(post.id)">
+          <img class="w-full object-cover aspect-square rounded-lg" :src="post.picture" />
+        </button>
+        <PublicationModal v-if="isPublicationModalShown" @close="closePublicationModal" :publication="shownPublication" />
+      </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
-import type { SimplifiedPost } from "@/api/type";
+import type { SimplifiedPost, Comment, Publication, SimplifiedUser } from "@/api/type";
 import CrossIcon from "@/components/icons/CrossIcon.vue";
-import PublicationModal from "@/components/modals/PublicationModalAlex.vue";
+import PublicationModal from "@/components/modals/PublicationModal.vue";
 
-function openPublicationModal(index: number) {
-  showVisitModals.value[index] = true;
-}
-const closePublicationModal = (index: number) => {
-  console.log(index);
-  showVisitModals.value[index] = false;
-  console.log(showVisitModals.value);
+const isPublicationModalShown = ref(false);
+const shownPublication = ref();
+
+const openPublicationModal = (publicationId: string) => {
+  console.log(publicationId);
+  //fetch pub
+  shownPublication.value = post1Complete;
+  isPublicationModalShown.value = true;
+};
+const closePublicationModal = () => {
+  isPublicationModalShown.value = false;
 };
 const vFocus = {
   mounted: (el: { focus: () => any; }) => el.focus(),
@@ -79,13 +86,66 @@ const post9 = {
   picture:
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTToy1vrfJ_4td_O5ofsY7f_0FrmTPCmIUWg&usqp=CAU",
 } as SimplifiedPost;
+const user1 = {
+  username: "Blond141",
+  profile_picture:
+    "https://h5p.org/sites/default/files/h5p/content/1209180/images/file-6113d5f8845dc.jpeg",
+} as SimplifiedUser;
 
+const user2 = {
+  username: "Chinman69",
+  profile_picture:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Chinese_flag_%28Beijing%29_-_IMG_1104.jpg/220px-Chinese_flag_%28Beijing%29_-_IMG_1104.jpg",
+} as SimplifiedUser;
+
+const user3 = {
+  username: "Alex_Prudent",
+  profile_picture:
+    "https://i5.walmartimages.ca/images/Enlarge/061/024/999999-773554061024.jpg",
+} as SimplifiedUser;
+const com1 = {
+  id: "c1",
+  user: user2,
+  content: "wow!",
+  date: "11 mars 2023",
+  total_rate: 1,
+  user_rate: 1,
+} as Comment;
+
+const com2 = {
+  id: "c2",
+  user: user3,
+  content: "nice",
+  date: "12 mars 2023",
+  total_rate: 0,
+  user_rate: 0,
+} as Comment;
+const post1Complete = {
+  id: "pub1",
+  user: user1,
+  date: "10 mars 2023",
+  picture:
+    "https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?cs=srgb&dl=pexels-pixabay-206359.jpg&fm=jpg",
+  desc: "Belle photo au Canadaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!",
+  tags: ["USA"],
+  total_rate: 12,
+  user_rate: 0,
+  comments: [com1, com2],
+} as Publication;
 const posts = [post1, post2, post3, post4, post5, post6, post7, post8, post9];
 
-let showVisitModals = ref(Array(posts.length).fill(false));
+
 </script>
 <style>
 .border-color {
   border-color: var(--color-border);
+}
+
+#explore {
+  background-color: var(--color-background-mute);
+}
+
+.profile {
+  background-color: var(--color-background);
 }
 </style>
