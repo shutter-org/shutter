0a9fcf47cb7c26e75aa32b997f66d2f635e1ead6
Jérôme <jerome.levesque.14@ulaval.ca>
2023-03-21 11:48:58 -0400

    fix : remove vue infinite scroll

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,11 @@ import router from "./router";
 import "./index.css";
 import "./assets/main.css";
 import piniaPluginPersistedState from "pinia-plugin-persistedstate";
-import infiniteScroll from "vue3-infinite-scroll-good";
 import VueDragscroll from "vue-dragscroll";
 
 const app = createApp(App);
 const pinia = createPinia();
 
-app.use(infiniteScroll)
 pinia.use(piniaPluginPersistedState);
 app.use(pinia);
 app.use(router);
